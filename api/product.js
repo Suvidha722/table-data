7eac1ad4b03336e9a1920ed60368c8fc0ead7d44
Suvidha Danaboina <43032+sdanaboina@users.noreply.git.corp.adobe.com>
2022-05-11 10:05:57 +0530

    set headers , CORS origin issue resolved

--- api/product.js
+++ api/product.js
@@ -1,43 +1,58 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get("/columns" , async (req, res) => {
-    try {
-        res.json([
-            { name: 'Name', uid: 'name' },
-            { name: 'Type', uid: 'type' },
-            { name: 'Date Modified', uid: 'date' },
-          ]);
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).send("Server error");
-    }
+router.get("/columns", async (req, res) => {
+  try {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    res.json([
+      { name: "Name", uid: "name" },
+      { name: "Type", uid: "type" },
+      { name: "Date Modified", uid: "date" },
+    ]);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server error");
+  }
 });
 
-
-router.get("/rows" , async (req, res) => {
-    try {
-        res.json([
-            { id: 1, name: 'Games', date: '6/7/2020', type: 'File folder' },
-            {
-              id: 2,
-              name: 'Program Files',
-              date: '4/7/2021',
-              type: 'File folder',
-            },
-            { id: 3, name: 'bootmgr', date: '11/20/2010', type: 'System file' },
-            {
-              id: 4,
-              name: 'log.txt',
-              date: '1/18/2016',
-              type: 'Text Document',
-            },
-          ]);
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).send("Server error");
-    }
+router.get("/rows", async (req, res) => {
+  try {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    res.json([
+      { id: 1, name: "Games", date: "6/7/2020", type: "File folder" },
+      {
+        id: 2,
+        name: "Program Files",
+        date: "4/7/2021",
+        type: "File folder",
+      },
+      { id: 3, name: "bootmgr", date: "11/20/2010", type: "System file" },
+      {
+        id: 4,
+        name: "log.txt",
+        date: "1/18/2016",
+        type: "Text Document",
+      },
+    ]);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server error");
+  }
 });
 module.exports = router;
